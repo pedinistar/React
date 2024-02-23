f500fa8767bafaa894e223e8dc73e550cf0fb378
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-02-23 11:48:10 +0000

    afternoon practice

---
+++ react-for-youtube/app/page.js
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+
+const page = () => {
+  us;
+  return (
+    // fragments
+    <>
+      <h1 className="font-bold text-xl">My total marks were {marks}</h1>
+      <button
+        onClick={() => {}}
+        className="bg-gray-400 px-5 py-2 rounded my-6 font-bold text-white"
+      >
+        Update
+      </button>
+    </>
+  );
+};
+
+export default page;
